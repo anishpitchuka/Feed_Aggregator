352b799d0c93e47c4f38429f75ce7fcc82500099
anishpitchuka <anishpitchuka8@gmail.com>
2025-09-01 14:48:02 +0000

    add postgres and drizzle

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,11 @@
+import { time } from "console";
+import { isNotNull } from "drizzle-orm";
+import { pgTable, uuid, timestamp, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow()
+                .$onUpdate(() => new Date()),
+    name: text("name").notNull().unique(),
+});
